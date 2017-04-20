9e4d6cc2270a5df808d50f44057d2495ec6eecd1
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-20 15:27:44 +0100

    forum.c feito e comentado, header limpo, mudei nomes de variaveis para estar mais explitico, main.c feito

--- proj1/header.h
+++ proj1/header.h
@@ -4,14 +4,32 @@
 #define MAX_USR 1000
 #define MAX_MSG 10000
 
+// ESTRUTURAS
+
 typedef struct msg {
-    unsigned short uid; // < 1000 && >= 0
-    char frase[MAX_FRASE + 1];
+    unsigned short sender_id; // < 1000 && >= 0
+    char message[MAX_FRASE + 1];
 } Msg;
 
-unsigned short u[MAX_USR];
-Msg m[MAX_MSG];
 
-int command_A();
+// VARIAVEIS UNIVERSAIS
+
+unsigned short user_list[MAX_USR];
+Msg message_list[MAX_MSG];
+
+
+// PROTOTIPOS #####CHECKAR SE SAO REALMENTE TODAS VOID OU NAO##### 
+
+
+int forum();
+    void execute_A(); 
+    void execute_L();
+    void execute_U();
+    void execute_O();
+    void execute_T();
+    void execute_S();
+    void execute_C();
+    void execute_X();
+
 
 #endif
