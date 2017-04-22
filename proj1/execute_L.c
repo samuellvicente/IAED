9875bea8b7c9ba2460e4ced20886d6c3c55bf1ba
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-22 17:52:17 +0100

    fixed warning

--- proj1/execute_L.c
+++ proj1/execute_L.c
@@ -5,5 +5,5 @@ void execute_L(){
     unsigned short a;
 
     printf("*TOTAL MESSAGES:%hu\n", total_msg);
-    for(a = 0; message_list[a].message[0] != '\0'; printf("%hu:%s\n", message_list[a].user_id, message_list[a++].message));
+    for(a = 0; message_list[a].message[0] != '\0'; printf("%hu:%s\n", message_list[a].user_id, message_list[a].message), a++);
 }
