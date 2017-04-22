410b3d34dd5a491dfa109e529dff86c7b883c510
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-22 20:34:06 +0100

    done

--- proj1/execute_O.c
+++ proj1/execute_O.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include "header.h"
 
 void execute_O(){
-    
-    puts("pressed O");
-
+    for (unsigned short a = 0; message_list[a].message[0] != '\0'; strlen(message_list[a].message) == longest_msg ? printf("*LONGEST SENTENCE:%hu:%s\n", message_list[a].user_id, message_list[a].message), a++ : a++);
 }
--- proj1/execute_U.c
+++ proj1/execute_U.c
@@ -2,9 +2,9 @@
 #include "header.h"
 
 void execute_U(){
-    unsigned short user, a;
+    unsigned short user;
     
-    scanf("%hu", &user);
+    scanf("%hu\n", &user);
 
-    for(a = 0; message_list[a].message[0] != '\0'; message_list[a].user_id == user ? printf("%s\n", message_list[a++].message) : a++);
+    for(unsigned short a = 0; message_list[a].message[0] != '\0'; message_list[a].user_id == user ? printf("%s\n", message_list[a++].message) : a++);
 }
