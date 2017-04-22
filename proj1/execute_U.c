194ba4c8991d775ed523f3a782bd4ee2f452dc0b
rpbranco <rafa.p.branco@gmail.com>
2017-04-22 19:55:42 +0100

    done execute_U.c

--- proj1/execute_U.c
+++ proj1/execute_U.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 void execute_U(){
+    unsigned short user, a;
     
-    puts("pressed U");
+    scanf("%hu", &user);
 
+    for(a = 0; message_list[a].message[0] != '\0'; message_list[a].user_id == user ? printf("%s\n", message_list[a++].message) : a++);
 }
