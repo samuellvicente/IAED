bbf9a6a9b6dce02ea90c7df83f1e4cdd75b40046
Rafael Branco <rafa.p.branco@gmail.com>
2017-04-22 16:16:49 +0100

    execute_A.c completed

--- proj1/execute_A.c
+++ proj1/execute_A.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-void execute_A(){
-    
-    puts("pressed A");
+void execute_A() {
+    unsigned short a;
+    char c;
 
+    scanf("%hu", &message_list[index_msg].sender_id); // guarda sender_id
+    getchar();
+
+    for(a = 0; (c = getchar()) != '\n'; message_list[index_msg].message[a++] = c); // guarda msg
+    message_list[index_msg].message[a] = '\0';
+
+    if(a > longest_msg) // atualiza longest_msg
+        longest_msg = a;
+
+    ++user_list[message_list[index_msg++].sender_id];
 }
