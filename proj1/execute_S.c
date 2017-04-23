f4537480a6218c55c7dc3fdc3037dd6d948160c0
rpbranco <rafa.p.branco@gmail.com>
2017-04-23 11:31:53 +0100

    S working very nice (.Y.)

--- proj1/execute_S.c
+++ proj1/execute_S.c
@@ -4,5 +4,8 @@
 
 void execute_S(){
     for(int a = 0; a < MAX_MSG; index_copy[a] = a, a++); // cria copia de index do vector das mensagens REVER!!!!!
-    // quicksort(index_copy)
+
+    quicksort(index_copy, 0, total_msg - 1, message_list);
+
+    for(int a = 0; a < total_msg; printf("%s\n", message_list[index_copy[a++]].message));
 }
