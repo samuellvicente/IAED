43cf13f3bfb21cecf8f7525ae3b6d5d96fbc8da2
rpbranco <rafa.p.branco@gmail.com>
2017-04-23 02:59:39 +0100

    execute_C test macro

--- proj1/execute_C.c
+++ proj1/execute_C.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "header.h"
 
+#define wchar(A) (( A < 'a' || A > 'z' ) && ( A < 'A' || A > 'Z' ))
+
 void execute_C(){
     unsigned int cnt = 0, a, i, j;
     char s[MAX_FRASE + 1];
@@ -9,9 +11,9 @@ void execute_C(){
     
     for(a = 0; message_list[a].message[0] != '\0' && a < MAX_MSG; a++)
         for(i = 0; message_list[a].message[i] != '\0'; i++)
-            if(message_list[a].message[i] < 'a' || message_list[a].message[i] > 'z' && message_list[a].message[i] < 'A' || message_list[a].message[i] > 'Z' || i == 0) {
+            if(wchar(message_list[a].message[i]) || i == 0) {
                 for(j = 0; message_list[a].message[i+j] == s[j]; j++);
-                if(s[j] == '\0' && (message_list[a].message[i] < 'a' || message_list[a].message[i] > 'z' && message_list[a].message[i] < 'A' || message_list[a].message[i] > 'Z'))
+                if(s[j] == '\0' && wchar(message_list[a].message[i]))
                     cnt++;
             }
     printf("%d", cnt);
