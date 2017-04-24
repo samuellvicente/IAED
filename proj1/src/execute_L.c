ca441997df164dbe36feacb3c05b606e326a3456
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-24 20:40:34 +0100

    removed structure

--- proj1/src/execute_L.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "header.h"
-
-void execute_L(){
-    printf("*TOTAL MESSAGES:%hu\n", total_msg);
-
-    for(unsigned int a = 0; message_list[a].message[0] != '\0' && a < MAX_MSG; printf("%hu:%s\n", message_list[a].user_id, message_list[a].message), a++);
-}
