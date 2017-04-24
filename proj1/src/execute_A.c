d57fd9fb66990b21fe35a96de04d64893b2d04b2
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-24 23:01:02 +0100

    Revert "removed structure"
    This reverts commit ca441997df164dbe36feacb3c05b606e326a3456.

---
+++ proj1/src/execute_A.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "header.h"
+
+void execute_A() {
+    unsigned short a;
+    char c;
+
+    scanf("%hu ", &message_list[index_msg].user_id); // guarda user_id PERGUNTAR AO PROF 
+
+    for(a = 0; (c = getchar()) != '\n'; message_list[index_msg].message[a++] = c); // guarda msg
+    message_list[index_msg].message[a] = '\0';
+
+    if(a > longest_msg) // atualiza longest_msg
+        longest_msg = a;
+
+    ++user_list[message_list[index_msg++].user_id];
+    ++total_msg;
+}
---
+++ proj1/src/execute_O.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include "header.h"
+
+void execute_O(){
+    for (unsigned int a = 0; message_list[a].message[0] != '\0' && a < MAX_MSG; strlen(message_list[a].message) == longest_msg ? printf("*LONGEST SENTENCE:%hu:%s\n", message_list[a].user_id, message_list[a].message), a++ : a++);
+}
