da1744b18b52310d12aeb37d9e884484b4f95c3d
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-25 21:05:11 +0100

    added eficiency

--- proj1/execute_S.c
+++ proj1/execute_S.c
@@ -3,9 +3,10 @@
 #include "header.h"
 
 void execute_S(){ 
-    for(unsigned int a = 0; a < MAX_MSG; index_copy[a] = a, a++); // cria copia de index do vector das mensagens REVER!!!!! POSSIBILIDADE DE OPTIMIZACAO se a < total_msg -1
+    for(unsigned int a = 0; a < total_msg; index_copy[a] = a, a++); // cria copia de index do vector das mensagens
 
     quicksort(index_copy, 0, total_msg - 1);
+
     printf("*SORTED MESSAGES:%hu\n", total_msg);
     for(int a = 0; a < total_msg; printf("%hu:%s\n",message_list[index_copy[a]].user_id, message_list[index_copy[a]].message), a++);
 }
