d6cf037277dd1d09a2b1092e7f6a1e7d8b576bc0
Rafael Branco <rafa.p.branco@gmail.com>
2017-04-26 21:53:03 +0100

    mais bonito mas ainda nao e aceite

--- proj1/execute_C.c
+++ proj1/execute_C.c
@@ -14,13 +14,12 @@ void execute_C() {
     for(a = 0; a < total_msg; a++)
         for(i = 0; message_list[a].message[i] != '\0'; i++)
             if(wchar(message_list[a].message[i]) || i == 0) {
-		for( ; wchar(message_list[a].message[i]); i++);
-                //while(wchar(message_list[a].message[i])) i++;
-		if(wchar(message_list[a].message[i+slen])) {
-                	for(j = 0; message_list[a].message[i+j] == s[j] && s[j] != '\0'; j++);
-                	if(s[j] == '\0' && wchar(message_list[a].message[i+j]))
-                    		cnt++;
-		}
+		        for( ; wchar(message_list[a].message[i]); i++); // avanca ate encontrar um char nao branco
+		        if(wchar(message_list[a].message[i+slen])) {
+                    for(j = 0; message_list[a].message[i+j] == s[j] && s[j] != '\0'; j++);
+                    if(s[j] == '\0' && wchar(message_list[a].message[i+j]))
+                        cnt++;
+		        }
             }
     printf("*WORD %s:%u\n", s, cnt);
 }
