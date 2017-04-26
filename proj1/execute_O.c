7e3aac69547715c31d5e8a60677258471296cf3e
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-26 10:34:25 +0100

    C99

--- proj1/execute_O.c
+++ proj1/execute_O.c
@@ -3,5 +3,6 @@
 #include "header.h"
 
 void execute_O(){
-    for (unsigned int a = 0; message_list[a].message[0] != '\0' && a < MAX_MSG; strlen(message_list[a].message) == longest_msg ? printf("*LONGEST SENTENCE:%hu:%s\n", message_list[a].user_id, message_list[a].message), a++ : a++);
+    unsigned int a; 
+    for (a = 0; message_list[a].message[0] != '\0' && a < MAX_MSG; strlen(message_list[a].message) == longest_msg ? printf("*LONGEST SENTENCE:%hu:%s\n", message_list[a].user_id, message_list[a].message), a++ : a++);
 }
