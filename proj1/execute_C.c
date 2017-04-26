fe091f0ef1f69a25bd7ae751540d91006b7c13d3
Rafael Branco <rafa.p.branco@gmail.com>
2017-04-26 19:46:05 +0100

    fixed 15

--- proj1/execute_C.c
+++ proj1/execute_C.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "header.h"
 
-void execute_C(){
+void execute_C() {
     unsigned int cnt = 0, a, i, j;
     char s[MAX_FRASE + 1];
 
@@ -11,8 +11,7 @@ void execute_C(){
     for(a = 0; message_list[a].message[0] != '\0' && a < MAX_MSG; a++)
         for(i = 0; message_list[a].message[i] != '\0'; i++)
             if(wchar(message_list[a].message[i]) || i == 0) {
-                if (wchar(message_list[a].message[i]))
-                    ++i;
+		for(; wchar(message_list[a].message[i]); i++);
                 for(j = 0; message_list[a].message[i+j] == s[j] && s[j] != '\0'; j++);
                 if(s[j] == '\0' && wchar(message_list[a].message[i+j]))
                     cnt++;
