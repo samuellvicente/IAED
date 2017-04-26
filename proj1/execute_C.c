860861f7bf8869a641f3731a30e0bc131a754cd2
Rafael Branco <rafa.p.branco@gmail.com>
2017-04-26 20:55:59 +0100

    probs fixed

--- proj1/execute_C.c
+++ proj1/execute_C.c
@@ -2,20 +2,25 @@
 #include "header.h"
 
 void execute_C() {
-    unsigned int cnt = 0, a, i, j;
+    unsigned int cnt = 0, a, i, j, slen;
     char s[MAX_FRASE + 1];
 
-    scanf("%s", s);
-    getchar(); 
+    //scanf("%s", s);
+    //getchar();
+    
+    for(slen = 0; (s[slen] = getchar()) != '\n'; slen++); // guarda msg
+    s[slen] = '\0';
 
     for(a = 0; a < total_msg; a++)
         for(i = 0; message_list[a].message[i] != '\0'; i++)
             if(wchar(message_list[a].message[i]) || i == 0) {
-		        //for( ; wchar(message_list[a].message[i]); i++);
-                while(wchar(message_list[a].message[i])) i++;
-                for(j = 0; message_list[a].message[i+j] == s[j] && s[j] != '\0'; j++);
-                if(s[j] == '\0' && wchar(message_list[a].message[i+j]))
-                    cnt++;
+		for( ; wchar(message_list[a].message[i]); i++);
+                //while(wchar(message_list[a].message[i])) i++;
+		if(wchar(message_list[a].message[i+a])) {
+                	for(j = 0; message_list[a].message[i+j] == s[j] && s[j] != '\0'; j++);
+                	if(s[j] == '\0' && wchar(message_list[a].message[i+j]))
+                    		cnt++;
+		}
             }
     printf("*WORD %s:%u\n", s, cnt);
 }
