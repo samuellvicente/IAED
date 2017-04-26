09491c1e7a6f8321a11bd423fb7d51d7d4b3583c
rpbranco <rafa.p.branco@gmail.com>
2017-04-26 10:37:47 +0100

    C99

--- proj1/execute_S.c
+++ proj1/execute_S.c
@@ -2,11 +2,13 @@
 #include <string.h>
 #include "header.h"
 
-void execute_S(){ 
-    for(unsigned int a = 0; a < total_msg; index_copy[a] = a, a++); // cria copia de index do vector das mensagens
+void execute_S() {
+    unsigned int a;
+
+    for(a = 0; a < total_msg; index_copy[a] = a, a++); // cria copia de index do vector das mensagens
 
     quicksort(index_copy, 0, total_msg - 1);
 
     printf("*SORTED MESSAGES:%hu\n", total_msg);
-    for(int a = 0; a < total_msg; printf("%hu:%s\n",message_list[index_copy[a]].user_id, message_list[index_copy[a]].message), a++);
+    for(a = 0; a < total_msg; printf("%hu:%s\n",message_list[index_copy[a]].user_id, message_list[index_copy[a]].message), a++);
 }
