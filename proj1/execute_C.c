b37f352646f14ae11b7e152d468ae6bd7cd28fbe
Rafael Branco <rafa.p.branco@gmail.com>
2017-04-26 21:58:35 +0100

    mais bonito mas ainda nao e aceite

--- proj1/execute_C.c
+++ proj1/execute_C.c
@@ -10,16 +10,16 @@ void execute_C() {
     
     for(slen = 0; (s[slen] = getchar()) != '\n'; slen++); // guarda msg
     s[slen] = '\0';
-
-    for(a = 0; a < total_msg; a++)
-        for(i = 0; message_list[a].message[i] != '\0'; i++)
-            if(wchar(message_list[a].message[i]) || i == 0) {
-		        for( ; wchar(message_list[a].message[i]); i++); // avanca ate encontrar um char nao branco
-		        if(wchar(message_list[a].message[i+slen])) {
-                    for(j = 0; message_list[a].message[i+j] == s[j] && s[j] != '\0'; j++);
-                    if(s[j] == '\0' && wchar(message_list[a].message[i+j]))
+	
+	for(a = 0; a < total_msg; a++)
+		for(i = 0; message_list[a].message[i] != '\0'; i++)
+			if(wchar(message_list[a].message[i]) || i == 0) {
+				for( ; wchar(message_list[a].message[i]); i++); // avanca ate encontrar um char nao branco
+				if(wchar(message_list[a].message[i+slen])) {
+					for(j = 0; message_list[a].message[i+j] == s[j] && s[j] != '\0'; j++);
+					if(s[j] == '\0' && wchar(message_list[a].message[i+j]))
                         cnt++;
-		        }
-            }
-    printf("*WORD %s:%u\n", s, cnt);
+				}
+			}
+	printf("*WORD %s:%u\n", s, cnt);
 }
