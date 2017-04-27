7f0594e29b42ae6d984f71dadf8ef4147c49341a
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-27 11:13:00 +0100

    FIXED CARALHO 16

--- proj1/execute_C.c
+++ proj1/execute_C.c
@@ -14,7 +14,7 @@ void execute_C() {
 	for(a = 0; a < total_msg; a++)
 		for(i = 0; message_list[a].message[i] != '\0'; i++)
 			if(wchar(message_list[a].message[i]) || i == 0) {
-				for( ; wchar(message_list[a].message[i]); i++); // avanca ate encontrar um char nao branco
+				for( ; wchar(message_list[a].message[i]) && message_list[a].message[i] != '\0'; i++); // avanca ate encontrar um char nao branco
 				if(wchar(message_list[a].message[i+slen])) {
 					for(j = 0; message_list[a].message[i+j] == s[j] && s[j] != '\0'; j++);
 					if(s[j] == '\0' && wchar(message_list[a].message[i+j]))
