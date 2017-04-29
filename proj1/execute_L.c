1497e4a929364e205e51998b8c031d0e42eaa51b
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-29 17:37:21 +0100

    removed unnecessary iterations

--- proj1/execute_L.c
+++ proj1/execute_L.c
@@ -4,5 +4,5 @@
 void execute_L(){
     unsigned int a; 
     printf("*TOTAL MESSAGES:%u\n", total_msg);
-    for(a = 0; message_list[a].message[0] != '\0' && a < MAX_MSG; printf("%hu:%s\n", message_list[a].user_id, message_list[a].message), a++);
+    for(a = 0; a < total_msg; printf("%hu:%s\n", message_list[a].user_id, message_list[a].message), a++);
 }
