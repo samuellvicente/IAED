3c89a088efae6c117bbc85744fd33b54d3290a90
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-29 18:26:42 +0100

    unpacked and replaced strlen

--- proj1/execute_O.c
+++ proj1/execute_O.c
@@ -3,6 +3,9 @@
 #include "header.h"
 
 void execute_O(){
-    unsigned int a; 
-    for (a = 0; message_list[a].message[0] != '\0' && a < MAX_MSG; strlen(message_list[a].message) == longest_msg ? printf("*LONGEST SENTENCE:%hu:%s\n", message_list[a].user_id, message_list[a].message), a++ : a++);
+    unsigned int a;
+
+    for (a = 0; a < total_msg; a++)
+        if(message_list[a].lenght_msg == longest_msg)
+            printf("*LONGEST SENTENCE:%hu:%s\n", message_list[a].user_id, message_list[a].message);
 }
