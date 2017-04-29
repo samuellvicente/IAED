6fd375404a76aa5c3e58c8cf75741ef933b00d14
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2017-04-29 18:27:13 +0100

    added enter

--- proj1/execute_L.c
+++ proj1/execute_L.c
@@ -2,7 +2,8 @@
 #include "header.h"
 
 void execute_L(){
-    unsigned int a; 
+    unsigned int a;
+
     printf("*TOTAL MESSAGES:%u\n", total_msg);
     for(a = 0; a < total_msg; printf("%hu:%s\n", message_list[a].user_id, message_list[a].message), a++);
 }
